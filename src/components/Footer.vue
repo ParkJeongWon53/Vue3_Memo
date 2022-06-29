24455984f3f787df06d6fc03e6f3c07c39f9fb90
ParkJeongWon53 <qkrwjd53@naver.com>
2022-06-29 17:12:34 +0900

    메모장

---
+++ src/components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+    <footer>&copy;2022 Memo App</footer>
+</template>
+
+<style scoped>
+footer{
+    background:#f7f7f7;
+    border-top: 1px solid #eee;
+    padding:25px;
+}
+</style>
---
+++ src/components/Memo.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="memo">
+        <div class="act">
+            <button class="btn btn-primary" @click="add()">+ 추가</button>
+        </div>
+        
+        <ul>
+            <li v-for="(d, idx) in state.data" :key="idx">{{ d }}</li>
+        </ul>
+    </div>
+</template>
+
+
+<script>
+import {reactive} from "vue"
+    export default {
+      
+        setup() {
+          const state = reactive({
+            data : []
+          })
+            const add = ()=>{
+              state.data.push("추가된 내용")
+            }
+            return {
+                state,
+                add
+            }
+        }
+    }
+</script>
+
+
+
+<style lang="scss" scoped>
+.memo { 
+  .act {
+    text-align: right;
+    padding: 10px 10px 5px 5px;
+
+  }
+  ul {
+    list-style: none;
+    padding: 15px;
+    margin: 0;
+    li {
+      padding: 15px;
+      margin: 5px;
+      border: 1px solid #eee;
+    }
+  }  
+}
+
+</style>
